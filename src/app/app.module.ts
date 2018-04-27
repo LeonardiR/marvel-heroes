2563da9816367bd24d471cbccb790ebc376cd74c
Regulo Leonardi <regulo.leonardipirela@altran.com>
2018-04-27 16:40:04 +0200

    Pagination module component test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 
 import { AppComponent } from './app.component';
@@ -28,6 +29,7 @@ import { PaginationComponent } from './pagination/pagination.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [
     HeroService,
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,6 +5,7 @@ import {Hero} from '../hero';
 import { HeroService } from '../hero.service';
 
 
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,6 +13,7 @@ import { HeroService } from '../hero.service';
 })
 
 export class HeroesComponent implements OnInit {
+  p = 1;
   heroes: Hero;
   modifiedSince = '';
   resultsPerPages = 20;
